ce113720492f062a8d97482ff146980e4ff2d229
Liam Bell <courseemaily@gmail.com>
2022-02-10 12:07:15 +1000

    Fixed minor bug in searchReducer

--- src/reducers/searchReducer.js
+++ src/reducers/searchReducer.js
@@ -4,19 +4,19 @@ export default function searchReducer(state = {}, action) {
   switch (action.type) {
     case SEARCH:
       return action.payload;
+
     case SAVE_RATING:
-      const rating = action.payload;
       const stateCopy = JSON.parse(JSON.stringify(state));
-      console.log('SEARCH_SAVE_RATING - state:', state);
-      console.log('SEARCH_SAVE_RATING - rating:', rating);
       if (stateCopy && stateCopy.movies && stateCopy.movies.length) {
         stateCopy.movies.map(movie => {
-          if (movie.imdbID === rating.imdbId) movie.rating = rating;
+          if (movie.imdbID === action.payload.imdbId) {
+            movie.rating = action.payload;
+          }
           return movie;
         }); 
       }
-      console.log('SEARCH_SAVE_RATING - stateCopy:', stateCopy);
       return stateCopy;
+      
     default:
       return state;
   }
