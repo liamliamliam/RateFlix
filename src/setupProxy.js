ef250dc57902a8a93c9a583b154cd744a1839902
Liam Bell <courseemaily@gmail.com>
2022-03-21 14:15:23 +1000

    changed setupProxy.js to handle dynamic forwarding

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -1,17 +1,22 @@
 const { createProxyMiddleware } = require('http-proxy-middleware');
 
+const url = {
+  development: 'localhost:5500',
+  production: 'https://rateflix-server.herokuapp.com'
+};
+
 module.exports = function(app) {
   app.use(
     '/api',
     createProxyMiddleware({
-      target: 'http://localhost:5500',
+      target: url[process.env.NODE_ENV],
       changeOrigin: true,
     })
   );
   app.use(
     '/auth',
     createProxyMiddleware({
-      target: 'http://localhost:5500',
+      target: url[process.env.NODE_ENV],
       changeOrigin: true,
     })
   );
